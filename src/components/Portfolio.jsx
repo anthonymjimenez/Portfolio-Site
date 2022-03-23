b006743062d4ce53725fea971b6c457f476f99d3
anthonymjimenez <anthonyjim96@gmail.com>
2022-03-23 14:47:26 -0400

    continued updating portfolio data and styling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
-import Stack from "@material-ui/core/Stack";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
 import Typography from "@material-ui/core/Typography";
 import quicklist from "../images/quicklist.png";
-import reactquiz from "../images/reactquiz.png";
-import jmarket from "../images/jmarket.png";
-import calorie from "../images/calorie.png";
+import reactquiz from "../images/reactquiz-xl.png";
+import jmarket from "../images/jmarket-sm.png";
+import calorie from "../images/calories.png";
+import movie from "../images/movie-noms.png";
+import node from "../images/node.png";
 
 // import project2 from "../images/html-css-javascript.jpg";
 // import project3 from "../images/javascript-fullstack.jpg";
@@ -23,7 +26,7 @@ import calorie from "../images/calorie.png";
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
     background: "#36454F",
-    height: "100vh",
+    height: "100%",
     opacity: "0.8",
   },
   cardContainer: {
@@ -52,14 +55,16 @@ const projects = [
     
     Quicklist was written using React, Node, Express, MongoDB Atlas, Bootstrap, and Auth0.`,
     image: quicklist,
+    url: "https://listquick.netlify.app/",
   },
   {
     name: "React Quiz",
     description: `This application uses user input and data from Trivia DB (https://opentdb.com/api_config) to create a personalized quiz. 
-    I've been incrementally working on this project for over a year! Last major change I implemented involved removing bootstrap and creating CSS styling.
+    I've been incrementally working on this project for over a year! Last major change I implemented involved creating a report that is displayed to users after completing a quiz.
     React Quiz was written using Axios, React, ES6+, CSS, he(decoding library)
     `,
     image: reactquiz,
+    url: "https://yesnote.netlify.app/",
   },
   {
     name: "jmarket",
@@ -67,11 +72,24 @@ const projects = [
     jmarket was written using React, Ruby on Rails (API), Postgres, and Bootstrap.
     `,
     image: jmarket,
+    url: false,
   },
   {
     name: "Calorie Tracker",
     description: `Calorie Tracker calculates the Body Mass Index of a user based on their weight, height, and age input. Based on the results the application will recommend a daily calorie count. A user can also input calorie events (eating, exercising) that reduce or increase the calorie count. `,
     image: calorie,
+    url: "https://calories-counted.herokuapp.com/",
+  },
+  {
+    name: "Movie Nominator",
+    description: `This application searches through the OMDB API to generate nomination lists. Users can search for movies they want to combine into a nomination list. Once the nominees are selected they are saved into local storage so that the data persists in the browser.`,
+    image: movie,
+    url: "https://my-movie-nominator.netlify.app/",
+  },
+  {
+    name: "Amazon Parser",
+    description: `This node application scraps data from an amazon URL and returns product details. I fetched the HTML as a string and then parsed through the dom elements. This required an advanced knowledge of HTML and javascript. This technology was later used in quicklist.   `,
+    image: node,
   },
 ];
 
@@ -79,36 +97,38 @@ const Portfolio = () => {
   const classes = useStyles();
   return (
     <Box component="div" className={classes.mainContainer}>
-      <Stack spacing={2}>
+      <Grid container>
         {/* Projects */}
         {projects.map((project, i) => (
-          <Card className={classes.cardContainer}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Project 1"
-                image={project.image}
-              />
-              <CardContent>
-                <Typography variant="h5" gutterBottom>
-                  {project.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  {project.description}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Live Demo
-              </Button>
-            </CardActions>
-          </Card>
+          <Grid item xs={12} lg={6} xl={4}>
+            <Card variant="outlined" className={classes.cardContainer}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Project 1"
+                  image={project.image}
+                />
+                <CardContent>
+                  <Typography variant="h5" gutterBottom>
+                    {project.name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary">
+                    {project.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Live Demo
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         ))}
-      </Stack>{" "}
+      </Grid>{" "}
     </Box>
   );
 };
