2bcd55fc6d87b28edcb4daccf6c49f9de21956fb
anthonymjimenez <anthonyjim96@gmail.com>
2022-03-28 14:14:48 -0400

    added emailjs for contact needs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,9 @@ import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-
 import Send from "@material-ui/icons/Send";
+import emailjs from "emailjs-com";
+import { init } from "@emailjs/browser";
 
 const useStyles = makeStyles((theme) => ({
   contactContainer: {
@@ -64,45 +65,66 @@ const InputField = withStyles({
 
 const Contact = () => {
   const classes = useStyles();
+
+  const sendEmail = async (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    try {
+      let results = await emailjs.sendForm(
+        "gmail",
+        process.env.REACT_APP_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_EMAIL_ID
+      );
+      console.log(results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Box component="div" className={classes.contactContainer}>
-      <Grid container justify="center">
-        <Box component="form" className={classes.form}>
-          <Typography variant="h5" className={classes.heading}>
-            Contact me!
-          </Typography>
-          <InputField
-            fullWidth={true}
-            label="Name"
-            variant="outlined"
-            inputProps={{ className: classes.input }}
-          />
-          <InputField
-            fullWidth={true}
-            label="Email"
-            variant="outlined"
-            inputProps={{ className: classes.input }}
-            className={classes.field}
-          />
-          <InputField
-            fullWidth={true}
-            label="Message"
-            variant="outlined"
-            multiline
-            rows={4}
-            inputProps={{ className: classes.input }}
-          />
-          <Button
-            variant="outlined"
-            fullWidth={true}
-            endIcon={<Send />}
-            className={classes.button}
-          >
-            Contact Me
-          </Button>
-        </Box>
-      </Grid>
-    </Box>
+    <>
+      {init(process.env.REACT_APP_EMAIL_ID)}{" "}
+      <Box component="div" className={classes.contactContainer}>
+        <Grid container justify="center">
+          <Box component="form" onSubmit={sendEmail} className={classes.form}>
+            <Typography variant="h5" className={classes.heading}>
+              Contact me!
+            </Typography>
+            <InputField
+              fullWidth={true}
+              label="Name"
+              variant="outlined"
+              inputProps={{ className: classes.input }}
+            />
+            <InputField
+              fullWidth={true}
+              label="Email"
+              variant="outlined"
+              inputProps={{ className: classes.input }}
+              className={classes.field}
+            />
+            <InputField
+              fullWidth={true}
+              label="Message"
+              variant="outlined"
+              multiline
+              rows={4}
+              inputProps={{ className: classes.input }}
+            />
+            <Button
+              variant="outlined"
+              fullWidth={true}
+              type="submit"
+              endIcon={<Send />}
+              className={classes.button}
+            >
+              Contact Me
+            </Button>
+          </Box>
+        </Grid>
+      </Box>
+    </>
   );
 };
 
